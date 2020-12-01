f384c039c8b625d7b12d781877a711e45b12b766
Kennedy <ibehkennedyj@gmail.com>
2020-11-30 22:54:02 -1000

    Day 1: Task 2

--- src/com/company/Day1.java
+++ src/com/company/Day1.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 
 public class Day1 {
 
-    public ArrayList<Integer> readInputs(){
+    public ArrayList<Integer> readInputs() {
         Scanner scanner = new Scanner(System.in);
         var list = new ArrayList<Integer>();
         var next = 0;
-        while (next != (-1)){
+        while (next != (-1)) {
             next = scanner.nextInt();
             list.add(next);
         }
         return list;
     }
 
-    private int[] getNumbers(){
+    private int[] getNumbers() {
         final ArrayList<Integer> inputs = readInputs();
-        for (int i = 0; i < inputs.size()-1; i++) {
+        for (int i = 0; i < inputs.size() - 1; i++) {
             for (int j = i + 1; j < inputs.size(); j++) {
-                if (inputs.get(i) + inputs.get(j) == 2020){
+                if (inputs.get(i) + inputs.get(j) == 2020) {
                     return new int[]{inputs.get(i), inputs.get(j)};
                 }
             }
@@ -28,8 +28,27 @@ public class Day1 {
         return null;
     }
 
-    public void printTask1(){
+    public void printTask1() {
         final int[] solution = getNumbers();
         System.out.println(solution[0] * solution[1]);
+    }
+
+    private int[] get3Numbers() {
+        final ArrayList<Integer> inputs = readInputs();
+        for (int i = 0; i < inputs.size() - 2; i++) {
+            for (int j = i + 1; j < inputs.size() - 1; j++) {
+                for (int k = j + 1; k < inputs.size(); k++) {
+                    if (inputs.get(i) + inputs.get(j) + inputs.get(k) == 2020) {
+                        return new int[]{inputs.get(i), inputs.get(j), inputs.get(k)};
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public void printTask2() {
+        final int[] solution = get3Numbers();
+        System.out.println(solution[0] * solution[1] * solution[2]);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        new Day1().printTask1();
+        new Day1().printTask2();
     }
 }
