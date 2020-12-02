8b52c7f2b2528990b4ec4ee7ae90833147f5f976
Kennedy <ibehkennedyj@gmail.com>
2020-12-02 00:25:13 -1000

    Day 2: Task 2

--- src/com/company/Day2.java
+++ src/com/company/Day2.java
@@ -1,33 +1,41 @@
 package com.company;
 
-import java.util.Arrays;
-
 import static java.lang.Integer.parseInt;
+import static java.lang.Math.toIntExact;
+import static java.lang.String.valueOf;
+import static java.util.Arrays.stream;
 
-public class Day2 implements Day{
+public class Day2 implements Day {
 
-    static class Password{
+    class Password {
         int x;
         int y;
         String marker;
         String password;
 
-         Password(String line){
+        Password(String line) {
             var parts = line.split(" ");
-            marker = String.valueOf(parts[1].charAt(0));
+            marker = valueOf(parts[1].charAt(0));
             password = parts[2];
             parts = parts[0].split("-");
             x = parseInt(parts[0]);
             y = parseInt(parts[1]);
         }
 
-        boolean isValid(){
-             long count = Arrays.stream(password.split(""))
-                     .filter(marker::equals)
-                     .count();
-             return x <= count && count <= y;
+        boolean isValid() {
+            long count = stream(password.split(""))
+                    .filter(marker::equals)
+                    .count();
+            return x <= count && count <= y;
+        }
+
+        boolean isValidTobaggan() {
+            int count = password.charAt(x - 1) == marker.charAt(0) ? 1 : 0;
+            count += (password.charAt(y - 1) == marker.charAt(0) ? 1 : 0);
+            return count == 1;
         }
     }
+
     @Override
     public int getDay() {
         return 2;
@@ -35,7 +43,7 @@ public class Day2 implements Day{
 
     @Override
     public Integer getTask1Solution() {
-        return Math.toIntExact(readLines(INPUT).stream()
+        return toIntExact(readLines(INPUT).stream()
                 .map(Password::new)
                 .filter(Password::isValid)
                 .count());
@@ -43,6 +51,9 @@ public class Day2 implements Day{
 
     @Override
     public Integer getTask2Solution() {
-        return null;
+        return toIntExact(readLines(INPUT).stream()
+                .map(Password::new)
+                .filter(Password::isValidTobaggan)
+                .count());
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,10 @@
 package com.company;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         new Day2().printSolutions();
-
-        System.out.println(Arrays.toString("abcde".split("a")));
     }
 }
