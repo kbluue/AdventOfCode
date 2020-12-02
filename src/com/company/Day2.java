8bf288c8bb8033e9646753e97aa66c2a399f45fd
Kennedy <ibehkennedyj@gmail.com>
2020-12-01 23:48:24 -1000

    Day 2: Task 1

--- src/com/company/Day2.java
+++ src/com/company/Day2.java
@@ -1,6 +1,33 @@
 package com.company;
 
+import java.util.Arrays;
+
+import static java.lang.Integer.parseInt;
+
 public class Day2 implements Day{
+
+    static class Password{
+        int x;
+        int y;
+        String marker;
+        String password;
+
+         Password(String line){
+            var parts = line.split(" ");
+            marker = String.valueOf(parts[1].charAt(0));
+            password = parts[2];
+            parts = parts[0].split("-");
+            x = parseInt(parts[0]);
+            y = parseInt(parts[1]);
+        }
+
+        boolean isValid(){
+             long count = Arrays.stream(password.split(""))
+                     .filter(marker::equals)
+                     .count();
+             return x <= count && count <= y;
+        }
+    }
     @Override
     public int getDay() {
         return 2;
@@ -8,7 +35,10 @@ public class Day2 implements Day{
 
     @Override
     public Integer getTask1Solution() {
-        return null;
+        return Math.toIntExact(readLines(INPUT).stream()
+                .map(Password::new)
+                .filter(Password::isValid)
+                .count());
     }
 
     @Override
