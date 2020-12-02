8be7a9fcb2f023d02dfc6c75a7ece70b406eba9c
Kennedy <ibehkennedyj@gmail.com>
2020-12-01 23:20:11 -1000

    feat: add initial day 2 resources

---
+++ src/com/company/Day2.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Day2 implements Day{
+    @Override
+    public int getDay() {
+        return 2;
+    }
+
+    @Override
+    public Integer getTask1Solution() {
+        return null;
+    }
+
+    @Override
+    public Integer getTask2Solution() {
+        return null;
+    }
+}
