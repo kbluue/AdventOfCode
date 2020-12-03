f186762bc6adddf57652cc7560aa97847ea8cfc0
Kennedy <ibehkennedyj@gmail.com>
2020-12-02 22:11:36 -1000

    Day 3: Task 2

--- src/com/company/Day3.java
+++ src/com/company/Day3.java
@@ -1,10 +1,12 @@
 package com.company;
 
-import java.util.stream.Collectors;
+import java.util.List;
 
-public class Day3 implements Day{
+import static java.util.stream.Collectors.toList;
 
-    class Row{
+public class Day3 implements Day {
+
+    class Row {
 
         String value;
 
@@ -12,11 +14,12 @@ public class Day3 implements Day{
             this.value = value;
         }
 
-        boolean hasTreeAt(int pos){
+        boolean hasTreeAt(int pos) {
             final int length = value.length();
-            return value.charAt(pos%(length)) == '#';
+            return value.charAt(pos % (length)) == '#';
         }
     }
+
     @Override
     public int getDay() {
         return 3;
@@ -24,18 +27,27 @@ public class Day3 implements Day{
 
     @Override
     public Integer getTask1Solution() {
-        var rows = readLines(INPUT).stream().map(Row::new).collect(Collectors.toList());
-        int y = 0;
-        int trees = 0;
-        for (Row row : rows) {
-            trees += row.hasTreeAt(y) ? 1 : 0;
-            y += 3;
-        }
-        return trees;
+        var rows = readLines(TEST).stream().map(Row::new).collect(toList());
+        return travel(rows, 3, 1);
     }
 
     @Override
     public Integer getTask2Solution() {
-        return null;
+        var rows = readLines(INPUT).stream().map(Row::new).collect(toList());
+        return travel(rows, 1, 1)
+                * travel(rows, 3, 1)
+                * travel(rows, 5, 1)
+                * travel(rows, 7, 1)
+                * travel(rows, 1, 2);
+    }
+
+    private int travel(List<Row> rows, int right, int down) {
+        int x = 0;
+        int trees = 0;
+        for (int y = 0; y < rows.size(); y += down) {
+            trees += rows.get(y).hasTreeAt(x) ? 1 : 0;
+            x += right;
+        }
+        return trees;
     }
 }
